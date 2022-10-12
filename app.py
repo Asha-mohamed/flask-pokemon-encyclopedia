60df550a7ab129a8ccf970e30859a45879eff1b1
Asha Mohamed <asha.mohamed27@hotmail.co.uk>
2022-10-12 23:40:23 +0100

    Refactor pokemon data endpoints to use external API

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask
+import requests
 
 
 app = Flask(__name__)
 
 # created a dictionary with all the data so we dont have all seperate datas
-pokemon_creature = {
-    "bulbasur": "dino",
-    "charmander": "reptile",
-    "pikachu": "rodent",
-    "eevee": "fox",
-    "diglett": "mole"
-}
+# pokemon_creature = {
+#     "bulbasur": "dino",
+#     "charmander": "reptile",
+#     "pikachu": "rodent",
+#     "eevee": "fox",
+#     "diglett": "mole"
+# } (removed this after as we wont need it. will use the pokemon api instead to fetch all data)
 
 @app.get("/") #.get is they want to get info/.post is they want
 # to provide some new info. / if the root of out app which means
@@ -19,16 +20,20 @@ def pokemon_list():
     return "bulbasur, charmander, pikachu, eevee, diglett"
 
 
-@app.get("/bulbasur")
-def bulbasur_data():
-    return "this is bulbasur, a generation 1 pokemon who looks like a dino"
+
 
 # after we create the dictionary
 # (if we want to insert a variable we put it in angle brackets).
+# NEW endpoint with pokemon API
 @app.get("/<pokemon_name>")
 def pokemon_data(pokemon_name):
-    creature = pokemon_creature.get(pokemon_name)
-    return f"this is {pokemon_name}, a generation 1 pokemon who looks like {creature}"
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}")
+    pokemon = response.json()
+    return f"this is {pokemon['name']}.\n" \
+           f"Height: {pokemon['height']}.\n" \
+           f"Weight: {pokemon['weight']}.\n" \
+           f"Base experience: {pokemon['base_experience']}.\n" \
+           f"Type(s): {' and '.join(type_info['type']['name'] for type_info in pokemon['types'])}"
 
 
 # # @app.get("/charmander")
