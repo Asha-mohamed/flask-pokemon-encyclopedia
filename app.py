dddd4d5624febde45cb577886209dd32f12c42d3
Asha Mohamed <asha.mohamed27@hotmail.co.uk>
2022-10-12 16:59:48 +0100

    Consolidate pokemon data endpoints into a single function

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# created a dictionary with all the data so we dont have all seperate datas
+pokemon_creature = {
+    "bulbasur": "dino",
+    "charmander": "reptile",
+    "pikachu": "rodent",
+    "eevee": "fox",
+    "diglett": "mole"
+}
 
 @app.get("/") #.get is they want to get info/.post is they want
 # to provide some new info. / if the root of out app which means
@@ -15,25 +23,32 @@ def pokemon_list():
 def bulbasur_data():
     return "this is bulbasur, a generation 1 pokemon who looks like a dino"
 
-
-@app.get("/charmander")
-def charmander_data():
-    return "this is charmander, a generation 1 pokemon who looks like a reptile"
-
-
-@app.get("/pikachu")
-def pikachu_data():
-    return "this is pikachu, a generation 1 pokemon who looks like a rodent"
+# after we create the dictionary
+# (if we want to insert a variable we put it in angle brackets).
+@app.get("/<pokemon_name>")
+def pokemon_data(pokemon_name):
+    creature = pokemon_creature.get(pokemon_name)
+    return f"this is {pokemon_name}, a generation 1 pokemon who looks like {creature}"
 
 
-@app.get("/eevee")
-def eevee_data():
-    return "this is eevee, a generation 1 pokemon who looks like a fox"
-
-
-@app.get("/diglett")
-def diglett_data():
-    return "this is diglett, a generation 1 pokemon who looks like a mole"
+# # @app.get("/charmander")
+# # def charmander_data():
+#     return "this is charmander, a generation 1 pokemon who looks like a reptile"
+#
+#
+# @app.get("/pikachu")
+# def pikachu_data():
+#     return "this is pikachu, a generation 1 pokemon who looks like a rodent"
+#
+#
+# @app.get("/eevee")
+# def eevee_data():
+#     return "this is eevee, a generation 1 pokemon who looks like a fox"
+#
+#
+# @app.get("/diglett")
+# def diglett_data():
+#     return "this is diglett, a generation 1 pokemon who looks like a mole"
 
 
 
